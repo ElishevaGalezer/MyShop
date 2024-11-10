b31c799bdfb4cba387572e3c22dacb3462f4d0d9
327517587 <327517587@PC1-26>
2024-11-10 14:04:02 +0200

    Add project files.

---
+++ Repositories/UserRepositories.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.Json;
+using Entities;
+namespace Repositories
+{
+    public class UserRepositories
+    {
+        public UserRepositories()
+        {
+
+        }
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        } 
+        public string Get(int id)
+        {
+            return "value";
+        }
+        public User Login( string UserName,  string Password)
+        {
+            using (StreamReader reader = System.IO.File.OpenText("M:\\web api\\MyShop\\MyShop\\UserList.txt"))
+            {
+                string? currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.UserName == UserName && user.Password == Password)
+                        return user;
+                }
+            }
+            return null;
+        }
+        public User Post(User user)
+        {
+            int numberOfUsers = System.IO.File.ReadLines("M:\\web api\\MyShop\\MyShop\\UserList.txt").Count();
+            user.userId = numberOfUsers + 1;
+            string userJson = JsonSerializer.Serialize(user);
+            System.IO.File.AppendAllText("M:\\web api\\MyShop\\MyShop\\UserList.txt", userJson + Environment.NewLine);
+            return user;
+        }
+
+        public User Put(int id, User userToUpdate)
+        {
+            string textToReplace = string.Empty;
+            userToUpdate.userId = id;
+            using (StreamReader reader = System.IO.File.OpenText("M:\\web api\\MyShop\\MyShop\\UserList.txt"))
+            {
+                string currentUserInFile;
+                while ((currentUserInFile = reader.ReadLine()) != null)
+                {
+                    User user = JsonSerializer.Deserialize<User>(currentUserInFile);
+                    if (user.userId == id)
+                        textToReplace = currentUserInFile;
+                }
+            }
+            if (textToReplace != string.Empty)
+            {
+                string text = System.IO.File.ReadAllText("M:\\web api\\MyShop\\MyShop\\UserList.txt");
+                text = text.Replace(textToReplace, JsonSerializer.Serialize(userToUpdate));
+                System.IO.File.WriteAllText("M:\\web api\\MyShop\\MyShop\\UserList.txt", text);
+                return userToUpdate;
+            }
+            return null;
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using Entities;
+using System.Text.Json;
+using Repositories;
+
+namespace Services
+{
+    public class UserService
+
+    {
+        UserRepositories repository = new();
+        public UserService()
+        { }
+
+            public IEnumerable<string> Get()
+            {
+                return new string[] { "value1", "value2" };
+            }
+            public string Get(int id)
+            {
+                return "value";
+            }
+            public User Login(string UserName, string Password)
+            {
+                return repository.Login(UserName, Password);
+            }
+            public User Post(User user)
+            {
+                
+                return repository.Post(user);
+
+            }
+
+            public User Put(int id, User userToUpdate)
+            {
+            return repository.Put(id, userToUpdate);
+            }
+
+            public void Delete(int id)
+            {
+            repository.Delete(id);
+        }
+        }
+    }
+
+ 
